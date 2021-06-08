bd56ec5af818f8cf2c0841c2412873f8ce92c0ba
iSuperman <sup.github@outlook.com>
2021-06-08 11:08:40 +0800

    优化为默认提取最新commit信息

--- shell/commit_formate.sh
+++ shell/commit_formate.sh
@@ -11,8 +11,7 @@
 #
 
 now=$(date +"%Y-%m-%d")
-num=0
-
+recent_date=""
 
 if [[ "$#" < 2 ]]
 then
@@ -20,16 +19,13 @@ then
   exit 1
 fi
 
-if [[ "$3" != "" ]]
+if [[ "$3" == "" ]]
 then
-  now=$3
-else
-	echo "未提供日期参数，默认使用当天日期"
+	echo "未提供日期参数，默认获取最新commit信息"
 fi
 
 # 获取所有Commit信息保存到 get_commit.log
 curl -so get_commit.log "https://api.github.com/repos/$1/$2/commits" 
-
 sed -i 's/\[//' get_commit.log
 sed -i 's/\]//' get_commit.log
 sed -i 's#{##' get_commit.log
@@ -39,51 +35,52 @@ sed -i 's/^[ \t]*//' get_commit.log
 sed -i ':a;N;$!ba;s/\n//g' get_commit.log
 
 grep -Po '"commit":.*?(?=tree)' get_commit.log > str_commit.log
+grep -Po '(?<="date": ").*?(?=T)' str_commit.log > date_commit_list.log
+
+while read line
+do
+	recent_date="$line"
+	break
+done < "./date_commit_list.log"
 
 while read line
 do	
-	result=$(echo $line | grep "$now")
+	result=$(echo $line | grep "$recent_date")
+	
 	if [[ "$result" != "" ]]
 	then 
 		echo $line >> day.log
-		num=$(($num+1))	
 	else
 		break
 	fi
 	
 done < "./str_commit.log"
-if [[ "$num" != 0 ]]
-then 
-	grep -Po '(?<="message": ").*?(?=")' day.log > day2.log
-	sed -i 's#).*$#)#g' day2.log
 
-	dayy=1
+grep -Po '(?<="message": ").*?(?=")' day.log > day2.log
+sed -i 's#).*$#)#g' day2.log
 
-	while read line
-	do
-		if [[ "$dayy" == 1 ]]
-		then
-			dayy=$(($dayy+1))
-			echo "- ${line}" >> day3.log
-		else
-			echo "\n- ${line}" >> day3.log
-		fi
-	done < "./day2.log"
-	# 去掉回车换行
-	sed -i ':a;N;$!ba;s/\n//g' day3.log
+dayy=1
 
-	# 防止转义-
-	sed -i 's/\-/\\\-/g' day3.log
-	sed -i 's/\./\\\./g' day3.log
-	sed -i 's/(/\\(/g' day3.log
-	sed -i 's/)/\\)/g' day3.log
-	sed -i 's/\#/\\#/g' day3.log
-	sed -i 's/<[^>]*>//g' day3.log
-	sed -i 's/\*\+//g' day3.log
-	sed -i 's#\\([^)]*)##g' day3.log
-	sed -i 's/\_/\\\-/g' day3.log
-else
-	echo "- Nothing...." >> day3.log
-	sed -i 's/\./\\\./g' day3.log
-	sed -i 's/\-/\\\-/g' day3.log
-fi
+while read line
+do
+	if [[ "$dayy" == 1 ]]
+	then
+		dayy=$(($dayy+1))
+		echo "- ${line}" >> day3.log
+	else
+		echo "\n- ${line}" >> day3.log
+	fi
+done < "./day2.log"
+# 去掉回车换行
+sed -i ':a;N;$!ba;s/\n//g' day3.log
+
+# 防止转义-
+sed -i 's/\-/\\\-/g' day3.log
+sed -i 's/\./\\\./g' day3.log
+sed -i 's/(/\\(/g' day3.log
+sed -i 's/)/\\)/g' day3.log
+sed -i 's/\#/\\#/g' day3.log
+sed -i 's/<[^>]*>//g' day3.log
+sed -i 's/\*\+//g' day3.log
+sed -i 's#\\([^)]*)##g' day3.log
+sed -i 's/\_/\\\-/g' day3.log
