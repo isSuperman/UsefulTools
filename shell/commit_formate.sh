a9b955fa5f5fe9672438fa97cee7976f46056dbc
iSuperman <isSuperman@users.noreply.github.com>
2021-06-12 14:20:19 +0800

    fix

--- shell/commit_formate.sh
+++ shell/commit_formate.sh
@@ -78,16 +78,15 @@ sed -i ':a;N;$!ba;s/\n//g' day3.log
 
 # 防止转义-
 sed -i "s/${now_year}-//g" recent_date.log
-sed -i 's/\-/\\\./g' recent_date.log
-sed -i 's/\-/\\\-/g' day3.log
-sed -i 's/\./\\\./g' day3.log
+sed -i 's/\-/\\./g' recent_date.log
+sed -i 's/\-/\\-/g' day3.log
+sed -i 's/\./\\./g' day3.log		
 sed -i 's/(/\\(/g' day3.log
 sed -i 's/)/\\)/g' day3.log
 sed -i 's/\#/\\#/g' day3.log
 sed -i 's/<[^>]*>//g' day3.log
-sed -i 's/\</\\\</g' day3.log
-sed -i 's/\>/\\\>/g' day3.log
-sed -i 's/=/\\=/g' day3.log
+sed -i 's/\>\=/\\>\\=/g' day3.log
+sed -i 's/\<\=/\\<\\=/g' day3.log
 sed -i 's/\*\+//g' day3.log
 sed -i 's#\\([^)]*)##g' day3.log
-sed -i 's/\_/\\\-/g' day3.log
+sed -i 's/\_/\\-/g' day3.log
