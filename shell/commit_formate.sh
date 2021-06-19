74479608302e23f7443c4285a0aed1a35001a057
iSuperman <sup.github@outlook.com>
2021-06-20 00:40:34 +0800

    Update commit_formate.sh

--- shell/commit_formate.sh
+++ shell/commit_formate.sh
@@ -107,13 +107,7 @@ generate_info(){
 				dayy=$(($dayy+1))
 				if [[ "$dayy" == 1 ]]
 				then
-					if [ ! -f "./day3.log" ]
-					then
-						echo "\n- ${branch}:\n${nbs[dayy]} ${line}" >> day3.log
-					else
-						echo "\n\n- ${branch}:\n${nbs[dayy]} ${line}" >> day3.log
-					fi
-					
+					echo "\n\n- ${branch}:\n${nbs[dayy]} ${line}" >> day3.log					
 				else
 					echo "\n${nbs[dayy]} ${line}" >> day3.log
 				fi
