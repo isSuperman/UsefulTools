0723f8cac6c99a8d667c958cafd75a70f668303e
iSuperman <isSuperman@users.noreply.github.com>
2021-06-21 09:28:55 +0800

    fix

--- shell/commit_formate.sh
+++ shell/commit_formate.sh
@@ -137,6 +137,7 @@ formate_result(){
 	sed -i 's#\\([^)]*)##g' day3.log
 	sed -i 's/\_/\\-/g' day3.log
 	sed -i 's/&/and/g' day3.log
+	sed -i 's/https:\/\/github.com\//Github@/g' day3.log
 }
 
 # Call funtions
